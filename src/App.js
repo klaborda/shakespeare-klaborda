e67f7ffde01dfefdd446deee1d57ddb04c858ae4
Kevin Laborda <klaborda@gmail.com>
2021-04-09 20:42:46 -0600

    display reviews in cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Moment from 'react-moment';
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -23,7 +25,22 @@ function App() {
   return (
     <Container fluid>
       <Row>
-        <Col>{JSON.stringify(reviews, null, 4)}</Col>
+        <Col>
+          <h1>Shakespear Quote Reviews</h1>
+        </Col>
+      </Row>
+      <Row>
+        {reviews && reviews.map(review => 
+          <Card key={review.id} style={{ width: '12rem'}}>
+            <Card.Body>
+              <Card.Title>{review.author}</Card.Title>
+              <Card.Subtitle>Rating: {review.rating} @ <Moment format="YYYY/MM/DD">{review.publish_date}</Moment></Card.Subtitle>
+              <Card.Text>
+                {review.body}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
       </Row>
     </Container>
   );
