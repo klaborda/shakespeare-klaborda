9e57595244c8014ba91aa7270f3a17c887ccea05
Kevin Laborda <klaborda@gmail.com>
2021-04-09 19:25:23 -0600

    perform API call and dump JSON to UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = "https://shakespeare.podium.com/api/reviews";
 
 function App() {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    getReviews();
+  }, []);
+
+  const getReviews = () => {
+    fetch(API_URL, { headers: { 'x-api-key': API_KEY }})
+      .then((response) => response.json())
+      .then(
+        (data) => setReviews([...data]),
+        (error) => console.error(error)
+      );
+  };
+
   return (
     <Container fluid>
       <Row>
-        <Col>1 of 1</Col>
+        <Col>{JSON.stringify(reviews, null, 4)}</Col>
       </Row>
     </Container>
   );
